2f9d1dcd9fd932c0f8d2188525b2556bcbfffa16
Your Name <you@example.com>
2026-01-09 11:31:13 +0530

    pushing all files of apps folder

---
+++ apps/main.py
@@ -0,0 +1,19 @@
+#import os
+from fastapi import FastAPI
+
+app = FastAPI(title="Internal Platform Demo App")
+
+#APP_VERSION = os.getenv("APP_VERSION", "unknown")
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to Internal Developer Platform"}
+
+@app.get("/health")
+def health():
+    return {"status": "UP"}
+    
+@app.get("/version")
+def version():
+    return {"version": "v2.0.0"}
+    
